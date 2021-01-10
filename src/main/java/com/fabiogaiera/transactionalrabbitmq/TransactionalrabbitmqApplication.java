016130da2253651f8bc2d35ad82e8c847a893560
Fabio Gaiera <fagaiera@gmail.com>
2021-01-10 08:34:37 +0100

    Added main class

---
+++ src/main/java/com/fabiogaiera/transactionalrabbitmq/TransactionalrabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.fabiogaiera.transactionalrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalrabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalrabbitmqApplication.class, args);
+	}
+
+}
